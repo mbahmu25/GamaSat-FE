8cab0facfe61d195cf4bb2b1a0b479335dcfa758
ammar2350 <13ammar13098@gmail.com>
2024-09-29 12:39:26 +0700

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,72 @@
 // src/app.jsx
 import React from 'react';
 import { useState } from 'react';
+import "leaflet/dist/leaflet.css"
 // import Sidebar from './component/sidebar';
 import {Sidebar} from "primereact/sidebar"
 import { Calendar } from 'primereact/calendar';
+import { Slider } from 'primereact/slider';
+import { InputText } from "primereact/inputtext";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css"
+
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 function App() {
   const [active,setActive] = useState(false)
+  const [date, setDate] = useState(null);
+  const [value, setValue] = useState();
+
+
     return (
-    <div className="App">
-      <Sidebar visible={active} onHide={()=>{setActive(!active)}}>
-        <div className='{w-10 h-5 bg-[red]'>
-          
+    <div className="App h-[100vh]">
+      
+      {/* <button onClick={()=>{setActive(true)}}>
+        <div className={`m-2 w-8 h-8 bg-[red] rounded-lg outline outline-black outline-2 transition duration-300 ease-in-out `}>
+          <div className='w-full h-1 bg-white'></div>
         </div>
+      </button> */}
+      <Sidebar visible={active} onHide={()=>{setActive(!active)}}>
+        
       </Sidebar>
-      <button onClick={()=>{setActive(true)}}>
-        <div className={`w-8 h-8 bg-[red] transition duration-300 ease-in-out `}></div>
-      </button>
-      <div className="ml-64 p-8"> {/* Margin left sama dengan lebar sidebar */}
-        <h1 className="text-4xl font-bold">Main Content</h1>
-        <p>This is the page content</p>
+    
+      <div className="h-screen flex">
+        {/* Yellow Section */}
+        <div className="bg-yellow-400 w-full h-full" style={{ width: '15%'}}>
+          <div className='card flex flex-wrap gap-5 p-fluid m-2'>
+            <div className="flex-auto">
+              <label htmlFor="buttondisplay" className="font-bold block mb-2 text-center">
+                Calendar
+              </label>
+              <Calendar id="buttondisplay" value={date} onChange={(e) => setDate(e.value)} showIcon />
+              <label htmlFor="buttondisplay" className="font-bold block mt-2 mb-2 text-center">
+                Threshold
+              </label>
+              <div className="w-14rem">
+                    <InputText value={value} onChange={(e) => setValue(e.target.value)} maxLength={15} />
+                    <Slider value={value} onChange={(e) => setValue(e.value)} step={1} max={15} />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Green Section */}
+        <div className="bg-green-500 w-full h-full" style={{}}>
+          <div className='bg-green-300 w-full' style={{height: '80%'}}>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              {/* <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker> */}
+            </MapContainer>
+          </div>
+          <div className='bg-green-400 w-full' style={{height: '20%'}}></div>
+        </div>
+        {/* Blue Section */}
+        {/* <div className="bg-blue-500" style={{ height: '33.33%' }}></div> */}
       </div>
     </div>
   );
