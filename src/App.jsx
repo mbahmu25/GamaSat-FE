c3761e6d4b5e5e64e138c5ec1a874d0ff5ce5b6a
ammar2350 <13ammar13098@gmail.com>
2024-10-03 20:31:13 +0700

    tab menu

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,13 @@ function App() {
     return null;
   };
 
+  const [activeMenu, setActiveMenu] = useState('calendar');
+  const handleMenuClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+
+
     return (
     <div className="App h-[100vh]">
       
@@ -43,38 +50,71 @@ function App() {
     
       <div className="h-screen flex ">
         {/* Yellow Section */}
-        <div className="bg-yellow-400 w-full h-full" style={{ width: '15%'}}>
-          <div className='card flex flex-wrap gap-5 p-fluid m-2'>
-            <div className="flex-auto">
+        <div className="bg-yellow-400 w-full h-full" style={{ width: '20%'}}>
+          <div className="flex mb-6">
+            <button
+              onClick={() => handleMenuClick('calendar')}
+              className={`px-2 py-1 ${activeMenu === 'calendar' ? 'bg-yellow-400 text-white' : 'bg-yellow-500'}`}
+            >
+              Calendar
+            </button>
+            <button
+              onClick={() => handleMenuClick('threshold')}
+              className={`px-4 py-1 ${activeMenu === 'threshold' ? 'bg-yellow-400 text-white' : 'bg-yellow-500'}`}
+            >
+              Threshold
+            </button>
+            <button
+              onClick={() => handleMenuClick('overpass_date')}
+              className={`px-4 py-1 ${activeMenu === 'overpass_date' ? 'bg-yellow-400 text-white' : 'bg-yellow-500'}`}
+            >
+              overpass Date
+            </button>
+          </div>
+          
+          {activeMenu === 'calendar' && (
+            <div className='card p-fluid m-2'>
               <label htmlFor="buttondisplay" className="font-bold block mb-2 text-center">
                 Calendar
               </label>
               <Calendar id="buttondisplay" value={date} onChange={(e) => setDate(e.value)} showIcon />
-              <label htmlFor="buttondisplay" className="font-bold block mt-2 mb-2 text-center">
-                Threshold = {value}
-              </label>
-              <div className="w-14rem">
-                    {/* <InputText value={value} onChange={(e) => setValue(e.target.value)} maxLength={15} /> */}
-                    <Slider value={value} onChange={(e) => setValue(e.value)} step={1} max={15} />
-              </div>
             </div>
-          </div>
-          <ul>
-              Overpass Date:
-              {overpas.map((e,i)=>{
-                if (i%2==0)
-                  {
-                    return(<li className='bg-[blue] bg-opacity-25'>
-                      {e}
-                    </li>)
-                  }else{
-                    return(<li className='bg-[red] bg-opacity-25'>
-                      {e}
-                    </li>)
-                  }
-              })}
-            </ul>
+          )}
+
+          {activeMenu === 'threshold' && (
+              <div className='ml-5 mr-5'>
+                <label htmlFor="buttondisplay" className="font-bold block mt-2 mb-2 text-center">
+                  Threshold = {value}
+                </label>
+                <div className="w-14rem">
+                      {/* <InputText value={value} onChange={(e) => setValue(e.target.value)} maxLength={15} /> */}
+                      <Slider value={value} onChange={(e) => setValue(e.value)} step={1} max={15} />
+                </div>
+              </div>
+          )}
+
+          {activeMenu === 'overpass_date' && (
+              <div className='ml-3 mr-3'>
+                  <ul>
+                    Overpass Date:
+                    {overpas.map((e,i)=>{
+                      if (i%2==0)
+                        {
+                          return(<li className='bg-[blue] bg-opacity-25'>
+                            {e}
+                          </li>)
+                        }else{
+                          return(<li className='bg-[red] bg-opacity-25'>
+                            {e}
+                          </li>)
+                        }
+                    })}
+                  </ul>
+              </div>
+          )}
         </div>
+
+
         {/* Green Section */}
         <div className="bg-green-500 w-full h-full" style={{}}>
           <div className='bg-green-300 w-full' style={{height: '70%'}}>
