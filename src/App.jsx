f843a29ba0bba44b2b7880b9f23f126285260666
ammar2350 <13ammar13098@gmail.com>
2024-10-03 20:10:00 +0700

    leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // src/app.jsx
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import "leaflet/dist/leaflet.css"
 // import Sidebar from './component/sidebar';
 import {Sidebar} from "primereact/sidebar"
@@ -8,8 +7,8 @@ import { Calendar } from 'primereact/calendar';
 import { Slider } from 'primereact/slider';
 import { InputText } from "primereact/inputtext";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css"
-
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   const [active,setActive] = useState(false)
@@ -20,6 +19,15 @@ function App() {
     "2024-05-17",
     "2024-05-24",
   ]
+  const [position, setPosition] = useState([-7.770226086602716, 470.3778303793861]);
+  const MapClickHandler = () => {
+    useMapEvents({
+      click: (event) => {
+        setPosition([event.latlng.lat, event.latlng.lng]);
+      },
+    });
+    return null;
+  };
 
     return (
     <div className="App h-[100vh]">
@@ -43,47 +51,49 @@ function App() {
               </label>
               <Calendar id="buttondisplay" value={date} onChange={(e) => setDate(e.value)} showIcon />
               <label htmlFor="buttondisplay" className="font-bold block mt-2 mb-2 text-center">
-                Threshold
+                Threshold = {value}
               </label>
               <div className="w-14rem">
-                    <InputText value={value} onChange={(e) => setValue(e.target.value)} maxLength={15} />
+                    {/* <InputText value={value} onChange={(e) => setValue(e.target.value)} maxLength={15} /> */}
                     <Slider value={value} onChange={(e) => setValue(e.value)} step={1} max={15} />
               </div>
             </div>
           </div>
-        </div>
-        {/* Green Section */}
-        <div className="bg-green-500 w-full h-full" style={{}}>
-          <div className='bg-green-300 w-full' style={{height: '70%'}}>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {/* <Marker position={[51.505, -0.09]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker> */}
-            </MapContainer>
-          </div>
-          <div className='bg-green-400 flex-col w-full' style={{height: '30%'}}>
-            <ul>
+          <ul>
               Overpass Date:
               {overpas.map((e,i)=>{
                 if (i%2==0)
                   {
-                    return(<li className='bg-[blue]'>
+                    return(<li className='bg-[blue] bg-opacity-25'>
                       {e}
                     </li>)
                   }else{
-                    return(<li className='bg-[red]'>
+                    return(<li className='bg-[red] bg-opacity-25'>
                       {e}
                     </li>)
                   }
               })}
             </ul>
+        </div>
+        {/* Green Section */}
+        <div className="bg-green-500 w-full h-full" style={{}}>
+          <div className='bg-green-300 w-full' style={{height: '70%'}}>
+            <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <MapClickHandler />
+              <Marker position={position}>
+                <Popup>
+                  ({position[0]}, {position[1]})
+                </Popup>
+              </Marker>
+            </MapContainer>
           </div>
+          {/* <div className='bg-green-400 flex-col w-full' style={{height: '30%'}}>
+            
+          </div> */}
         </div>
         {/* Blue Section */}
         {/* <div className="bg-blue-500" style={{ height: '33.33%' }}></div> */}
